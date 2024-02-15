e93b9fc2cf67f7220f7bae2fddbfecaf30a01b54
JohnnyTaiCK <126487338+JohnnyTaiCK@users.noreply.github.com>
2024-02-15 21:20:35 +0800

    minor changes

--- FYP/Misinformation_Detector/api/views.py
+++ FYP/Misinformation_Detector/api/views.py
@@ -6,4 +6,4 @@ from rest_framework.response import Response
 class UserFeedbackViewSet(viewsets.ViewSet):
     
     def create(self,request):
-        return Response({'prediction': "祝戴澤健順利畢業，找到份好工作，最重要的是全心全意投入到學習編程，開始科學運動，找到女朋友，增值自己！"})
+        return Response({'prediction': "必勝！"})
